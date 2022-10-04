69f7ab7cc64ed4388066d7b87270521f6fbfa528
Tamru <tamrualem111@gmail.com>
2022-10-04 22:06:27 +0300

    added new files

---
+++ ahm.js
@@ -0,0 +1,1048 @@
+function playmusic()
+{
+    var music = new Audio('ሀ.mp3');
+    music.play();
+}
+function playmusic2()
+{
+    var music = new Audio('ሁ.mp3');
+    music.play();
+}
+function playmusic3()
+{
+    var music = new Audio('ሂ.mp3');
+    music.play();
+}
+function playmusic4()
+{
+    var music = new Audio('ሃ.mp3');
+    music.play();
+}
+function playmusic5()
+{
+    var music = new Audio('ሄ.mp3');
+    music.play();
+}
+function playmusic6()
+{
+    var music = new Audio('ህ.mp3');
+    music.play();
+}
+function playmusic7()
+{
+    var music = new Audio('ሆ.mp3');
+    music.play();
+}
+function playmusic8()
+{
+    var music = new Audio('ለ.mp3');
+    music.play();
+}
+function playmusic9()
+{
+    var music = new Audio('ሉ.mp3');
+    music.play();
+}
+function playmusic10()
+{
+    var music = new Audio('ሊ.mp3');
+    music.play();
+}
+function playmusic11()
+{
+    var music = new Audio('ላ.mp3');
+    music.play();
+}
+function playmusic12()
+{
+    var music = new Audio('ሌ.mp3');
+    music.play();
+}
+function playmusic13()
+{
+    var music = new Audio('ል.mp3');
+    music.play();
+}
+function playmusic14()
+{
+    var music = new Audio('ሎ.mp3');
+    music.play();
+}
+
+function playmusic15()
+{
+    var music = new Audio('መ.mp3');
+    music.play();
+}
+function playmusic16()
+{
+    var music = new Audio('ሙ.mp3');
+    music.play();
+}
+function playmusic17()
+{
+    var music = new Audio('ሚ.mp3');
+    music.play();
+}
+function playmusic18()
+{
+    var music = new Audio('ማ.mp3');
+    music.play();
+}
+function playmusic19()
+{
+    var music = new Audio('ሜ.mp3');
+    music.play();
+}
+function playmusic20()
+{
+    var music = new Audio('ም.mp3');
+    music.play();
+}
+function playmusic21()
+{
+    var music = new Audio('ሞ.mp3');
+    music.play();
+}
+
+
+function playmusic22()
+{
+    var music = new Audio('ሠ.mp3');
+    music.play();
+}
+function playmusic23()
+{
+    var music = new Audio('ሡ.mp3');
+    music.play();
+}
+function playmusic24()
+{
+    var music = new Audio('ሢ.mp3');
+    music.play();
+}
+function playmusic25()
+{
+    var music = new Audio('ሣ.mp3');
+    music.play();
+}
+function playmusic26()
+{
+    var music = new Audio('ሤ.mp3');
+    music.play();
+}
+function playmusic27()
+{
+    var music = new Audio('ሥ.mp3');
+    music.play();
+}
+function playmusic28()
+{
+    var music = new Audio('ሦ.mp3');
+    music.play();
+}
+
+function playmusic29()
+{
+    var music = new Audio('ረ.mp3');
+    music.play();
+}
+function playmusic30()
+{
+    var music = new Audio('ሩ.mp3');
+    music.play();
+}
+function playmusic31()
+{
+    var music = new Audio('ሪ.mp3');
+    music.play();
+}
+function playmusic32()
+{
+    var music = new Audio('ራ.mp3');
+    music.play();
+}
+function playmusic33()
+{
+    var music = new Audio('ሬ.mp3');
+    music.play();
+}
+function playmusic34()
+{
+    var music = new Audio('ር.mp3');
+    music.play();
+}
+function playmusic35()
+{
+    var music = new Audio('ሮ.mp3');
+    music.play();
+}
+
+function playmusic36()
+{
+    var music = new Audio('ሸ.mp3');
+    music.play();
+}
+function playmusic37()
+{
+    var music = new Audio('ሹ.mp3');
+    music.play();
+}
+function playmusic38()
+{
+    var music = new Audio('ሺ.mp3');
+    music.play();
+}
+function playmusic39()
+{
+    var music = new Audio('ሻ.mp3');
+    music.play();
+}
+function playmusic40()
+{
+    var music = new Audio('ሼ.mp3');
+    music.play();
+}
+function playmusic41()
+{
+    var music = new Audio('ሽ.mp3');
+    music.play();
+}
+function playmusic42()
+{
+    var music = new Audio('ሾ.mp3');
+    music.play();
+}
+
+function playmusic43()
+{
+    var music = new Audio('ቀ.mp3');
+    music.play();
+}
+function playmusic44()
+{
+    var music = new Audio('ቁ.mp3');
+    music.play();
+}
+function playmusic45()
+{
+    var music = new Audio('ቂ.mp3');
+    music.play();
+}
+function playmusic46()
+{
+    var music = new Audio('ቃ.mp3');
+    music.play();
+}
+function playmusic47()
+{
+    var music = new Audio('ቄ.mp3');
+    music.play();
+}
+function playmusic48()
+{
+    var music = new Audio('ቅ.mp3');
+    music.play();
+}
+function playmusic49()
+{
+    var music = new Audio('ቆ.mp3');
+    music.play();
+}
+
+function playmusic50()
+{
+    var music = new Audio('በ.mp3');
+    music.play();
+}
+function playmusic51()
+{
+    var music = new Audio('ቡ.mp3');
+    music.play();
+}
+function playmusic52()
+{
+    var music = new Audio('ቢ.mp3');
+    music.play();
+}
+function playmusic53()
+{
+    var music = new Audio('ባ.mp3');
+    music.play();
+}
+function playmusic54()
+{
+    var music = new Audio('ቤ.mp3');
+    music.play();
+}
+function playmusic55()
+{
+    var music = new Audio('ብ.mp3');
+    music.play();
+}
+function playmusic56()
+{
+    var music = new Audio('ቦ.mp3');
+    music.play();
+}
+
+function playmusic57()
+{
+    var music = new Audio('ተ.mp3');
+    music.play();
+}
+function playmusic58()
+{
+    var music = new Audio('ቱ.mp3');
+    music.play();
+}
+function playmusic59()
+{
+    var music = new Audio('ቲ.mp3');
+    music.play();
+}
+function playmusic60()
+{
+    var music = new Audio('ታ.mp3');
+    music.play();
+}
+function playmusic61()
+{
+    var music = new Audio('ቴ.mp3');
+    music.play();
+}
+function playmusic62()
+{
+    var music = new Audio('ት.mp3');
+    music.play();
+}
+function playmusic63()
+{
+    var music = new Audio('ቶ.mp3');
+    music.play();
+}
+
+function playmusic64()
+{
+    var music = new Audio('ቸ.mp3');
+    music.play();
+}
+function playmusic65()
+{
+    var music = new Audio('ቹ.mp3');
+    music.play();
+}
+function playmusic66()
+{
+    var music = new Audio('ቺ.mp3');
+    music.play();
+}
+function playmusic67()
+{
+    var music = new Audio('ቻ.mp3');
+    music.play();
+}
+function playmusic68()
+{
+    var music = new Audio('ቼ.mp3');
+    music.play();
+}
+function playmusic69()
+{
+    var music = new Audio('ች.mp3');
+    music.play();
+}
+function playmusic70()
+{
+    var music = new Audio('ቾ.mp3');
+    music.play();
+}
+
+function playmusic71()
+{
+    var music = new Audio('ነ.mp3');
+    music.play();
+}
+function playmusic72()
+{
+    var music = new Audio('ኑ.mp3');
+    music.play();
+}
+function playmusic73()
+{
+    var music = new Audio('ኒ.mp3');
+    music.play();
+}
+function playmusic74()
+{
+    var music = new Audio('ና.mp3');
+    music.play();
+}
+function playmusic75()
+{
+    var music = new Audio('ኔ.mp3');
+    music.play();
+}
+function playmusic76()
+{
+    var music = new Audio('ን.mp3');
+    music.play();
+}
+function playmusic77()
+{
+    var music = new Audio('ኖ.mp3');
+    music.play();
+}
+
+function playmusic78()
+{
+    var music = new Audio('ኘ.mp3');
+    music.play();
+}
+function playmusic79()
+{
+    var music = new Audio('ኙ.mp3');
+    music.play();
+}
+function playmusic80()
+{
+    var music = new Audio('ኚ.mp3');
+    music.play();
+}
+function playmusic81()
+{
+    var music = new Audio('ኛ.mp3');
+    music.play();
+}
+function playmusic82()
+{
+    var music = new Audio('ኜ.mp3');
+    music.play();
+}
+function playmusic83()
+{
+    var music = new Audio('ኝ.mp3');
+    music.play();
+}
+function playmusic84()
+{
+    var music = new Audio('ኞ.mp3');
+    music.play();
+}
+
+function playmusic85()
+{
+    var music = new Audio('አ.mp3');
+    music.play();
+}
+function playmusic86()
+{
+    var music = new Audio('ኡ.mp3');
+    music.play();
+}
+function playmusic87()
+{
+    var music = new Audio('ኢ.mp3');
+    music.play();
+}
+function playmusic88()
+{
+    var music = new Audio('ኣ.mp3');
+    music.play();
+}
+function playmusic89()
+{
+    var music = new Audio('ኤ.mp3');
+    music.play();
+}
+function playmusic90()
+{
+    var music = new Audio('እ.mp3');
+    music.play();
+}
+function playmusic91()
+{
+    var music = new Audio('ኦ.mp3');
+    music.play();
+}
+
+function playmusic92()
+{
+    var music = new Audio('ከ.mp3');
+    music.play();
+}
+function playmusic93()
+{
+    var music = new Audio('ኩ.mp3');
+    music.play();
+}
+function playmusic94()
+{
+    var music = new Audio('ኪ.mp3');
+    music.play();
+}
+function playmusic95()
+{
+    var music = new Audio('ካ.mp3');
+    music.play();
+}
+function playmusic96()
+{
+    var music = new Audio('ኬ.mp3');
+    music.play();
+}
+function playmusic97()
+{
+    var music = new Audio('ክ.mp3');
+    music.play();
+}
+function playmusic98()
+{
+    var music = new Audio('ኮ.mp3');
+    music.play();
+}
+
+function playmusic99()
+{
+    var music = new Audio('ኸ.mp3');
+    music.play();
+}
+function playmusic100()
+{
+    var music = new Audio('ኹ.mp3');
+    music.play();
+}
+function playmusic101()
+{
+    var music = new Audio('ኺ.mp3');
+    music.play();
+}
+function playmusic102()
+{
+    var music = new Audio('ኻ.mp3');
+    music.play();
+}
+function playmusic103()
+{
+    var music = new Audio('ኼ.mp3');
+    music.play();
+}
+function playmusic104()
+{
+    var music = new Audio('ኽ.mp3');
+    music.play();
+}
+function playmusic105()
+{
+    var music = new Audio('ኾ.mp3');
+    music.play();
+}
+
+function playmusic106()
+{
+    var music = new Audio('ወ.mp3');
+    music.play();
+}
+function playmusic107()
+{
+    var music = new Audio('ዉ.mp3');
+    music.play();
+}
+function playmusic108()
+{
+    var music = new Audio('ዊ.mp3');
+    music.play();
+}
+function playmusic109()
+{
+    var music = new Audio('ዋ.mp3');
+    music.play();
+}
+function playmusic110()
+{
+    var music = new Audio('ዌ.mp3');
+    music.play();
+}
+function playmusic111()
+{
+    var music = new Audio('ው.mp3');
+    music.play();
+}
+function playmusic112()
+{
+    var music = new Audio('ዎ.mp3');
+    music.play();
+}
+
+function playmusic113()
+{
+    var music = new Audio('ዘ.mp3');
+    music.play();
+}
+function playmusic114()
+{
+    var music = new Audio('ዙ.mp3');
+    music.play();
+}
+function playmusic115()
+{
+    var music = new Audio('ዚ.mp3');
+    music.play();
+}
+function playmusic116()
+{
+    var music = new Audio('ዛ.mp3');
+    music.play();
+}
+function playmusic117()
+{
+    var music = new Audio('ዜ.mp3');
+    music.play();
+}
+function playmusic118()
+{
+    var music = new Audio('ዝ.mp3');
+    music.play();
+}
+function playmusic119()
+{
+    var music = new Audio('ዞ.mp3');
+    music.play();
+}
+
+function playmusic120()
+{
+    var music = new Audio('ዠ.mp3');
+    music.play();
+}
+function playmusic121()
+{
+    var music = new Audio('ዡ.mp3');
+    music.play();
+}
+function playmusic122()
+{
+    var music = new Audio('ዢ.mp3');
+    music.play();
+}
+function playmusic123()
+{
+    var music = new Audio('ዣ.mp3');
+    music.play();
+}
+function playmusic124()
+{
+    var music = new Audio('ዤ.mp3');
+    music.play();
+}
+function playmusic125()
+{
+    var music = new Audio('ዥ.mp3');
+    music.play();
+}
+function playmusic126()
+{
+    var music = new Audio('ዦ.mp3');
+    music.play();
+}
+
+function playmusic127()
+{
+    var music = new Audio('የ.mp3');
+    music.play();
+}
+function playmusic128()
+{
+    var music = new Audio('ዩ.mp3');
+    music.play();
+}
+function playmusic129()
+{
+    var music = new Audio('ዪ.mp3');
+    music.play();
+}
+function playmusic130()
+{
+    var music = new Audio('ያ.mp3');
+    music.play();
+}
+function playmusic131()
+{
+    var music = new Audio('ዬ.mp3');
+    music.play();
+}
+function playmusic132()
+{
+    var music = new Audio('ይ.mp3');
+    music.play();
+}
+function playmusic133()
+{
+    var music = new Audio('ዮ.mp3');
+    music.play();
+}
+
+function playmusic134()
+{
+    var music = new Audio('ደ.mp3');
+    music.play();
+}
+function playmusic135()
+{
+    var music = new Audio('ዱ.mp3');
+    music.play();
+}
+function playmusic136()
+{
+    var music = new Audio('ዲ.mp3');
+    music.play();
+}
+function playmusic137()
+{
+    var music = new Audio('ዳ.mp3');
+    music.play();
+}
+function playmusic138()
+{
+    var music = new Audio('ዴ.mp3');
+    music.play();
+}
+function playmusic139()
+{
+    var music = new Audio('ድ.mp3');
+    music.play();
+}
+function playmusic140()
+{
+    var music = new Audio('ዶ.mp3');
+    music.play();
+}
+
+function playmusic141()
+{
+    var music = new Audio('ጀ.mp3');
+    music.play();
+}
+function playmusic142()
+{
+    var music = new Audio('ጁ.mp3');
+    music.play();
+}
+function playmusic143()
+{
+    var music = new Audio('ጂ.mp3');
+    music.play();
+}
+function playmusic144()
+{
+    var music = new Audio('ጃ.mp3');
+    music.play();
+}
+function playmusic145()
+{
+    var music = new Audio('ጄ.mp3');
+    music.play();
+}
+function playmusic146()
+{
+    var music = new Audio('ጅ.mp3');
+    music.play();
+}
+function playmusic147()
+{
+    var music = new Audio('ጆ.mp3');
+    music.play();
+}
+//
+function playmusic148()
+{
+    var music = new Audio('ገ.mp3');
+    music.play();
+}
+function playmusic149()
+{
+    var music = new Audio('ጉ.mp3');
+    music.play();
+}
+function playmusic150()
+{
+    var music = new Audio('ጊ.mp3');
+    music.play();
+}
+function playmusic151()
+{
+    var music = new Audio('ጋ.mp3');
+    music.play();
+}
+function playmusic152()
+{
+    var music = new Audio('ጌ.mp3');
+    music.play();
+}
+function playmusic153()
+{
+    var music = new Audio('ግ.mp3');
+    music.play();
+}
+function playmusic154()
+{
+    var music = new Audio('ጎ.mp3');
+    music.play();
+}
+
+function playmusic155()
+{
+    var music = new Audio('ጠ.mp3');
+    music.play();
+}
+function playmusic156()
+{
+    var music = new Audio('ጡ.mp3');
+    music.play();
+}
+function playmusic157()
+{
+    var music = new Audio('ጢ.mp3');
+    music.play();
+}
+function playmusic158()
+{
+    var music = new Audio('ጣ.mp3');
+    music.play();
+}
+function playmusic159()
+{
+    var music = new Audio('ጤ.mp3');
+    music.play();
+}
+function playmusic160()
+{
+    var music = new Audio('ጥ.mp3');
+    music.play();
+}
+function playmusic161()
+{
+    var music = new Audio('ጦ.mp3');
+    music.play();
+}
+
+function playmusic162()
+{
+    var music = new Audio('ጨ.mp3');
+    music.play();
+}
+function playmusic163()
+{
+    var music = new Audio('ጩ.mp3');
+    music.play();
+}
+function playmusic164()
+{
+    var music = new Audio('ጪ.mp3');
+    music.play();
+}
+function playmusic165()
+{
+    var music = new Audio('ጫ.mp3');
+    music.play();
+}
+function playmusic166()
+{
+    var music = new Audio('ጬ.mp3');
+    music.play();
+}
+function playmusic167()
+{
+    var music = new Audio('ጭ.mp3');
+    music.play();
+}
+function playmusic168()
+{
+    var music = new Audio('ጮ.mp3');
+    music.play();
+}
+
+
+function playmusic169()
+{
+    var music = new Audio('ጰ.mp3');
+    music.play();
+}
+function playmusic170()
+{
+    var music = new Audio('ጱ.mp3');
+    music.play();
+}
+function playmusic171()
+{
+    var music = new Audio('ጲ.mp3');
+    music.play();
+}
+function playmusic172()
+{
+    var music = new Audio('ጳ.mp3');
+    music.play();
+}
+function playmusic173()
+{
+    var music = new Audio('ጴ.mp3');
+    music.play();
+}
+function playmusic174()
+{
+    var music = new Audio('ጵ.mp3');
+    music.play();
+}
+function playmusic175()
+{
+    var music = new Audio('ጶ.mp3');
+    music.play();
+}
+
+
+function playmusic176()
+{
+    var music = new Audio('ጸ.mp3');
+    music.play();
+}
+function playmusic177()
+{
+    var music = new Audio('ጹ.mp3');
+    music.play();
+}
+function playmusic178()
+{
+    var music = new Audio('ጺ.mp3');
+    music.play();
+}
+function playmusic179()
+{
+    var music = new Audio('ጻ.mp3');
+    music.play();
+}
+function playmusic180()
+{
+    var music = new Audio('ጼ.mp3');
+    music.play();
+}
+function playmusic181()
+{
+    var music = new Audio('ጽ.mp3');
+    music.play();
+}
+function playmusic182()
+{
+    var music = new Audio('ጾ.mp3');
+    music.play();
+}
+
+
+function playmusic183()
+{
+    var music = new Audio('ፈ.mp3');
+    music.play();
+}
+function playmusic184()
+{
+    var music = new Audio('ፉ.mp3');
+    music.play();
+}
+function playmusic185()
+{
+    var music = new Audio('ፊ.mp3');
+    music.play();
+}
+function playmusic186()
+{
+    var music = new Audio('ፋ.mp3');
+    music.play();
+}
+function playmusic187()
+{
+    var music = new Audio('ፌ.mp3');
+    music.play();
+}
+function playmusic188()
+{
+    var music = new Audio('ፍ.mp3');
+    music.play();
+}
+function playmusic189()
+{
+    var music = new Audio('ፎ.mp3');
+    music.play();
+}
+
+
+function playmusic190()
+{
+    var music = new Audio('ፐ.mp3');
+    music.play();
+}
+function playmusic191()
+{
+    var music = new Audio('ፑ.mp3');
+    music.play();
+}
+function playmusic192()
+{
+    var music = new Audio('ፒ.mp3');
+    music.play();
+}
+function playmusic193()
+{
+    var music = new Audio('ፓ.mp3');
+    music.play();
+}
+function playmusic194()
+{
+    var music = new Audio('ፔ.mp3');
+    music.play();
+}
+function playmusic195()
+{
+    var music = new Audio('ፕ.mp3');
+    music.play();
+}
+function playmusic196()
+{
+    var music = new Audio('ፖ.mp3');
+    music.play();
+}
+
+
+function playmusic197()
+{
+    var music = new Audio('ቨ.mp3');
+    music.play();
+}
+function playmusic198()
+{
+    var music = new Audio('ቩ.mp3');
+    music.play();
+}
+function playmusic199()
+{
+    var music = new Audio('ቪ.mp3');
+    music.play();
+}
+function playmusic200()
+{
+    var music = new Audio('ቫ.mp3');
+    music.play();
+}
+function playmusic201()
+{
+    var music = new Audio('ቬ.mp3');
+    music.play();
+}
+function playmusic202()
+{
+    var music = new Audio('ቭ.mp3');
+    music.play();
+}
+function playmusic203()
+{
+    var music = new Audio('ቮ.mp3');
+    music.play();
+}
